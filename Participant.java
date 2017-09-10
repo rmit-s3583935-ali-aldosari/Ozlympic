d51a4552e3a8d20a50d66db19c890a2daa695c56
rmit-s3583935-ali-aldosari <mona@meems-MacBook-Air.local>
2017-09-10 13:26:44 +1000

    participant ver 1.0

---
+++ Participant.java
@@ -0,0 +1,29 @@
+
+package Ozlympic;
+
+
+public abstract class Participant {
+  protected String name;
+  protected String state;
+  protected int age;
+  protected int id;
+  
+  
+public String getName() {
+	return name;
+}
+public String getState() {
+	return state;
+}
+public int getAge() {
+	return age;
+}
+public int getId() {
+	return id;
+}
+
+  
+
+    
+    
+}
